f3b81f8d5fe78b937d61382c78303bed8b7b17cc
Viachaslau Kitsun <Viachaslau_Kitsun@epam.com>
2019-03-26 14:46:37 +0300

    Add project files.

---
+++ NetMentoring_HttpClient/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetMentoring_HttpClient
+{
+    public class Class1
+    {
+    }
+}
